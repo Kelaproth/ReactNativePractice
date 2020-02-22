391c22eb54bc48a1162dafe2deb9305fb3ea22d3
Kevin TANG <tth502025390@gmail.com>
2020-02-22 22:46:30 +0800

    Redux

---
+++ redux/configureStore.js
@@ -0,0 +1,26 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+// import { createForms } from 'react-redux-form';
+import { dishes } from './dishes';
+import { comments } from './comments';
+import { promotions } from './promotions';
+import { leaders } from './leaders';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+// import { InitialFeedback } from './forms';
+
+export const ConfigureStore = () => {
+    const store = createStore(
+        combineReducers({
+            dishes,
+            comments,
+            promotions,
+            leaders
+            // ...createForms({
+            //     feedback: InitialFeedback
+            // })
+        }),
+        applyMiddleware(thunk, logger)
+    );
+
+    return store;
+}
